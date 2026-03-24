17bb695c11b3276c8b167fe331b812b593c9cef5
YESICA CAROLINA LÓPEZ ORDOÑEZ <ylopezo8@miumg.edu.gt>
2026-03-24 16:37:21 -0600

    Practica de Initializer Block

--- src/mis/practicas/Dias.java
+++ src/mis/practicas/Dias.java
@@ -1,6 +1,6 @@
 package mis.practicas;
 
-public class Practica_Enum {
+public class Dias {
 	// Ejercicio de dias de la semana donde si es fin de semana diga dia de descanso y si no que diga que es dia de hacer tareas.
 	
 	public enum DiasDeLaSemana {
--- src/mis/practicas/Main.java
+++ src/mis/practicas/Main.java
@@ -1,16 +1,17 @@
 package mis.practicas;
 
-import mis.practicas.Practica_Enum.DiasDeLaSemana;
+import mis.practicas.Dias.DiasDeLaSemana;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ejercicio1();
+		ejercicioEnum();
+		ejercicioInitializerBlock();
 		
 	}
 
-	private static void ejercicio1() {
+	private static void ejercicioEnum() {
 		DiasDeLaSemana dia = DiasDeLaSemana.LUNES;
 	if (dia == DiasDeLaSemana.SABADO || dia == DiasDeLaSemana.DOMINGO ) {
 		System.out.println("Hoy " + dia + " es dia de descanso ");
@@ -20,5 +21,23 @@ public class Main {
 	}
 		
 	}
-
+	private static void ejercicioInitializerBlock() {
+		Estudiante estudiante1 = new Estudiante("Carla","Vega", 22);
+		estudiante1.asignarMaterias("Fisíca", "Quimica", "Ingles", "Matematicas");
+		
+		Estudiante estudiante2 = new Estudiante("Adrian","Ortiz", 21);
+		estudiante2.asignarMaterias("Español", "Historia", "Matematicas", "Quimica");
+		
+		Estudiante estudiante3 = new Estudiante("Alex","Martinez", 20);
+		estudiante3.asignarMaterias("Derecho", "Español", "Historia", "Sociales");
+		
+		Estudiante estudiante4 = new Estudiante("Veronica","Lemus", 23);
+		estudiante4.asignarMaterias("Fisíca", "Programacion", "Ingles", "Estadistica");
+		
+		estudiante1.mostrar();
+		estudiante2.mostrar();
+		estudiante3.mostrar();
+		estudiante4.mostrar();
+	}
+		
 }
